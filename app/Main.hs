289eaa330b680d86aacd8d6beb2c00956cf41835
Lupino <lmjubuntu@gmail.com>
2016-09-15 15:52:46 +0800

    update file api

--- app/Main.hs
+++ app/Main.hs
@@ -14,7 +14,11 @@ import Data.Default.Class (def)
 import Control.Monad.IO.Class (liftIO)
 import System.FilePath ((</>), dropDrive)
 import qualified Data.Text as T (pack, unpack)
+import qualified Data.Text.Lazy as TL (pack, unpack)
+import qualified Data.ByteString.Char8 as BC (unpack)
+import qualified Data.ByteString.Lazy.Char8 as BL
 import Data.Aeson (object, (.=))
+import Network.Mime (MimeType, defaultMimeLookup)
 
 main :: IO ()
 main = do
@@ -24,8 +28,16 @@ main = do
     middleware staticMid
 
     get "/api/file" $ do
-      trees <- liftIO $ getFileTreeList root
+      trees <- liftIO $ getFileTreeList $ root </> "source"
       json $ treeListToJSON trees
+
+    get (regex "^/api/file/(.*)") $ do
+      path <- filePath root
+      liftIO $ print path
+      let headers = [("Content-Type", getMimeType path)]
+      setHeader "Content-Type" $ TL.pack $ BC.unpack $ getMimeType path
+      fc <- liftIO $ BL.readFile path
+      raw fc
 
     put (regex "^/api/file/(.*)") $ do
       path <- filePath root
@@ -51,3 +63,8 @@ filePath root = do
   req <- request
   let path = foldr ((</>) . T.unpack) "" (drop 2 $ pathInfo req)
   return $ root </> "source" </> path
+
+
+-- | Guess MIME type from file extension
+getMimeType :: FilePath -> MimeType
+getMimeType = defaultMimeLookup . T.pack
